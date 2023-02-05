ef04f759ed0823168b7fdda462c9332033c74bb0
Alex Axthelm <alex@alexaxthelm.com>
2023-02-05 17:51:21 +0100

    Change run script to actually run PACTA

--- run_single_ssh.sh
+++ run_single_ssh.sh
@@ -1,6 +1,6 @@
 az vm create \
     --resource-group UST-2022 \
-    --name myAutomatedVM \
+    --name pacta-runner-ssh \
     --image UbuntuLTS \
     --assign-identity /subscriptions/feef729b-4584-44af-a0f9-4827075512f9/resourceGroups/UST-2022/providers/Microsoft.ManagedIdentity/userAssignedIdentities/PACTA-runner \
     --admin-username azureuser \
